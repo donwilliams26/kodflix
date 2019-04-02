d337879f65460a4d85ac0897bb82cd7278a92c7e
Don Williams <2donwilliams6@gmail.com>
2019-04-02 12:42:37 +0100

    Updated kodlfix welcome message for users

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to Kodflix to my Users 
+            Welcome to Kodflix you beautiful Users 
           </p>
           <a
             className="App-link"
