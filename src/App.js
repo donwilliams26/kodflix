52a5c55fe546e99720ae5927518dcb174ce5af14
Don Williams <2donwilliams6@gmail.com>
2019-04-02 23:31:10 +0100

    Replaced Shows tiltes with covers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cover from './images/championships.jpeg';
+import depapel from './images/casa.jpg';
+import gamers from './images/gamer.jpg';
+import hacker from './images/mrrobot.jpg';
+import ender from './images/enders.jpg';
+import letscook from './images/breakingbad.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <h1>Welcome to My kodflix</h1>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <br /><br /><br />
+        <div className='container'>
+          <div className='item'>
+            <img src={depapel} alt='money heist' />
+          </div>
+          <div className='item'>
+            <img src={gamers} alt='gamericon' />
+          </div>
+          <div className='item'>
+            <img src={hacker} alt='Mr.robot' />
+          </div>
+        </div>
+        <div className='container'>      
+          <div className='item'>
+            <img src={ender} alt='enders' />
+          </div>
+          <div className='item'>
+            <img src={letscook} alt='bbad' />
+          </div>
+          <div className='item'>
+            <img src={cover} alt='champion' />
+          </div>
+        </div>
       </div>
     );
   }
