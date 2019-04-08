19cb3d27bb0f9bb11a193a41c4182dfd4e5cf86f
Williams Tchoumbou <williamstchoumbou@Williamss-MacBook-Pro-2.local>
2019-04-08 13:42:36 +0100

    #9 Create a Gallery component to show the covers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-/*import cover from './images/championships.jpeg';
-import depapel from './images/casa.jpg';
-import gamers from './images/gamer.jpg';
-import hacker from './images/mrrobot.jpg';
-import ender from './images/enders.jpg';
-import letscook from './images/breakingbad.jpg';
-import Tvitem from './tvItem';*/
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Technologies from './Technologies';
-import Details from './Details';
-
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route exact path='/' component={Technologies} />
-          <Route exact path='/Details' component={Details} />
-        </div>
-      </Router>
+      <div className="App">
+        <br /><br /><br />
+        <Technologies />
+      </div>
     );
   }
 }
