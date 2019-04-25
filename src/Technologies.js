de172b1678251b1f69c5b0325370bacd0fdde236
Williams Tchoumbou <williamstchoumbou@Dannys-MBP.localdomain>
2019-04-25 19:44:06 +0100

    #11 Improve the gallery layout

--- src/Technologies.js
+++ src/Technologies.js
@@ -14,13 +14,9 @@ export default function Technologies() {
                 <Tvitem id='Casa de Papel' name='CASA de Papel' logo={depapel} />
                 <Tvitem id='Gamer' name='GAMER' logo={gamers} />
                 <Tvitem id='Mr. Robot' name='Mr. ROBOT' logo={hacker} />
-            </div>
-
-            <div className='container'>
                 <Tvitem id='Championship' name='CHAMPIONSHIP' logo={cover} />
                 <Tvitem id="Ender's Game" name="ENDER'S GAME" logo={ender} />
                 <Tvitem id='Breaking Bad' name='BREAKING BAD' logo={letscook} />
-
             </div>
         </div>
     );
