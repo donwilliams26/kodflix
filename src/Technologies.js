b24640a2041900901a39bed88f42a7fe3a70b595
Williams Tchoumbou <williamstchoumbou@Williamss-MacBook-Pro-2.local>
2019-05-19 02:41:19 +0100

    #12 Move the covers data model to a proper front-end data source

--- src/Technologies.js
+++ src/Technologies.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import cover from './images/championships.jpeg';
-import depapel from './images/casa.jpg';
-import gamers from './images/gamer.jpg';
-import hacker from './images/mrrobot.jpg';
-import ender from './images/enders.jpg';
-import letscook from './images/breakingbad.jpg';
-import Tvitem from './tvItem';
+import React from "react";
+import Stack from "./Stack";
+import getTechnologies from "./technologies-get";
 
 export default function Technologies() {
-    return (
-        <div>
-            <div className='container'>
-                <Tvitem id='Casa de Papel' name='CASA de Papel' logo={depapel} />
-                <Tvitem id='Gamer' name='GAMER' logo={gamers} />
-                <Tvitem id='Mr. Robot' name='Mr. ROBOT' logo={hacker} />
-                <Tvitem id='Championship' name='CHAMPIONSHIP' logo={cover} />
-                <Tvitem id="Ender's Game" name="ENDER'S GAME" logo={ender} />
-                <Tvitem id='Breaking Bad' name='BREAKING BAD' logo={letscook} />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="container">
+        {getTechnologies().map(stack => (
+          <Stack
+            key={stack.id}
+            id={stack.id}
+            name={stack.name}
+            logo={stack.logo}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-
