f7270f5bbfc660ee77bd505506f1c0b922c8e6f2
Williams Tchoumbou <williamstchoumbou@Dannys-MBP.localdomain>
2019-05-20 20:21:31 +0100

    Improve setup of backend server #20

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 const shows = require("./shows");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello Williams!"));
 app.get("/rest/shows", (req, res) => res.send(shows()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
