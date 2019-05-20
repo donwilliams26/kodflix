08c56d2525375310ee80900914f3024ae5541b22
Williams Tchoumbou <williamstchoumbou@Dannys-MBP.localdomain>
2019-05-20 19:55:04 +0100

    Return a list of shows from the backend #19

---
+++ src/backend/shows.js
@@ -0,0 +1,10 @@
+module.exports = function shows() {
+  return [
+    { id: "Casa de Papel", name: "CASA de Papel", synopsis: "depapel" },
+    { id: "Gamer", name: "GAMER", logo: "gamers" },
+    { id: "Mr. Robot", name: "MR. ROBOT", logo: "hacker" },
+    { id: "Championship", name: "CHAMPIONSHIP", synopsis: "cover" },
+    { id: "Ender's Game", name: "ENDER'S GAME", synopsis: "ender" },
+    { id: "Breaking Bad", name: "BREAKING BAD", synopsis: "letscook" }
+  ];
+};
